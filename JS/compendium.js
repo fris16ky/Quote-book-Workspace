1de36b3bda252d53762a8097ed0d18a09fb1f0d2
Logan Howard <hlogan235@gmail.com>
2023-11-23 18:01:40 -0500

    Retrying basic sql in JS since I accidentally had the wrong JS link in the HTML

--- JS/compendium.js
+++ JS/compendium.js
@@ -1,6 +1,4 @@
-const express = require("express");
 const sql = require("mssql");
-const app = express();
 
 const config = {
   user: "your_username",
@@ -10,29 +8,32 @@ const config = {
 };
 
 function PullData() {
-  // Make a GET request to the server endpoint
-  fetch("/get-quotes")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
+  // Connect to the database
+  sql
+    .connect(config)
+    .then(() => {
+      // Query
+      return sql.query`SELECT * FROM Quotebook`;
     })
-    .then((data) => {
+    .then((result) => {
       // Display results in the resultContainer
-      const resultContainer = document.querySelector(".quotesContainer");
+      const resultContainer = document.getElementById("resultContainer");
 
       // Clear previous results
       resultContainer.innerHTML = "";
 
       // Append new results
-      data.forEach((record) => {
+      result.recordset.forEach((record) => {
         const resultItem = document.createElement("div");
         resultItem.textContent = JSON.stringify(record);
         resultContainer.appendChild(resultItem);
       });
     })
-    .catch((error) => {
-      console.error("Error:", error);
+    .catch((err) => {
+      console.error("Error:", err);
+    })
+    .finally(() => {
+      // Close the connection
+      sql.close();
     });
 }
